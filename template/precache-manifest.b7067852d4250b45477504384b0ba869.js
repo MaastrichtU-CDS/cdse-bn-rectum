dc1302f840e60cba28d3e9065d3fd8f2255b57bb
Leroy Linssen <leroylinssen@gmail.com>
2021-07-19 13:01:17 +0200

    Add Melles static view implementation as example

---
+++ template/precache-manifest.b7067852d4250b45477504384b0ba869.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86b9448f44fd38419aa00d1bfeb97825",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a72264140bd2eaa62295",
+    "url": "/static/css/main.5ad7c339.chunk.css"
+  },
+  {
+    "revision": "7b39658a993e4737965f",
+    "url": "/static/js/2.cbcb8332.chunk.js"
+  },
+  {
+    "revision": "23a7c212b12caf7ab7d9dab48b39c2c2",
+    "url": "/static/js/2.cbcb8332.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a72264140bd2eaa62295",
+    "url": "/static/js/main.28ae88f0.chunk.js"
+  },
+  {
+    "revision": "68a9665f6992de0c4239",
+    "url": "/static/js/runtime-main.fc75219d.js"
+  }
+]);
